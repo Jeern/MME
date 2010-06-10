1e0b1fd4989779b17898e556c673940ce7a2c21b
Jern_cp <jesper@niedermann.dk>
2010-06-10 22:37:03 +0000

    Visibility of Build and GAC menu fixed. Now only shown when .snk file is present

--- MMETools/MenuManager.cs
+++ MMETools/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MMEContracts;
+using EnvDTE;
 
 namespace MMETools
 {
@@ -21,6 +22,7 @@ namespace MMETools
                 var seperator = new MenuItem("", true);
                 var buildAndGacMenu = new MenuItem("Build and add to GAC");
                 buildAndGacMenu.Click += BuildAndGacMenuClick;
+                buildAndGacMenu.IsVisible = BuildAndGacMenuVisible;
                 menus.Add(seperator);
                 menus.Add(buildAndGacMenu);
                 return menus;
@@ -31,6 +33,29 @@ namespace MMETools
         private void BuildAndGacMenuClick(object sender, EventArgs<IMenuContext> e)
         {
             e.Data.Details.VSStudio.ExecuteCommand("Build.BuildSelection");
+        }
+
+        private bool BuildAndGacMenuVisible(IMenuContext context)
+        {
+            if (context.Details.Project == null)
+                return false;
+
+            ProjectItems items = context.Details.Project.ProjectItems;
+            if (items == null)
+                return false;
+
+            if (items.Count <= 0)
+                return false;
+
+            for (int i = 1; i <= items.Count; i++)
+            {
+                if (items.Item(i).Name.EndsWith(".snk"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }

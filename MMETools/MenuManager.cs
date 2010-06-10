6ef8671f78efa5b8affcc8e8aa0089f3f1a375ee
Jern_cp <jesper@niedermann.dk>
2010-06-10 15:45:42 +0000

    New Tools project

---
+++ MMETools/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMEContracts;
+
+namespace MMETools
+{
+    public class MenuManager : IMenuManager
+    {
+        public string MainMenu()
+        {
+            return "Managed Menu Extensions";
+        }
+
+        public IEnumerable<IMenuItem> GetMenus(ContextLevels menuForLevel)
+        {
+            var menus = new List<MenuItem>();
+            if (menuForLevel == ContextLevels.Project)
+            {
+                var seperator = new MenuItem("", true);
+                var buildAndGacMenu = new MenuItem("Build and add to GAC");
+                buildAndGacMenu.Click += BuildAndGacMenuClick;
+                menus.Add(seperator);
+                menus.Add(buildAndGacMenu);
+                return menus;
+            }
+            return menus;
+        }
+
+        private void BuildAndGacMenuClick(object sender, EventArgs<IMenuContext> e)
+        {
+            e.Data.Details.VSStudio.ExecuteCommand("");
+        }
+    }
+}
